0e817b10b8a116a7eeb6a94b2d836a14666a313f
ludekg <grusludek@gmail.com>
2022-12-22 16:24:35 +0100

    Add files via upload

---
+++ src/main/java/com/lgrus/SandBoxTests/SandBoxTestsApplication.java
@@ -0,0 +1,13 @@
+package com.lgrus.SandBoxTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandBoxTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandBoxTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lgrus/SandBoxTests/SandBoxTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgrus.SandBoxTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandBoxTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
